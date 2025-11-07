623d5dc4619fff4fd20632c4d457f03895baa1b4
Your Name <you@example.com>
2025-11-07 11:59:47 +0100

    Milestone 1: - Create project and setup with Vite - Added react-router-dom as a dependency - Added Bootstrap as a dependency in package.json and package-lock.json - Created Navbar component for page navigation - Added AddTask and TaskList components - Updated App.jsx to include routing with React Router - Removed unused index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
 import './App.css'
+import AddTask from './pages/AddTask'
+import TaskList from './pages/TaskList'
+import Navbar from './components/Navbar'
 
 function App() {
 
 
   return (
     <>
-
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/task-list" element={<TaskList />} />
+          <Route path="/add-task" element={<AddTask />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+export default function Navbar() {
+    return (
+        <>
+            <nav className='navbar navbar-dark bg-dark'>
+                <div className="container">
+                    <NavLink className='navbar-brand' to='/'>
+                        My App
+                    </NavLink>
+                    <div>
+                        <NavLink className='nav-link d-inline text-white me-3' to='/'>
+                            Lista Task
+                        </NavLink>
+                        <NavLink className='nav-link d-inline text-white' to='/add-task'>
+                            Aggiungi Task
+                        </NavLink>
+                    </div>
+                </div >
+            </nav >
+        </>
+    )
+}
